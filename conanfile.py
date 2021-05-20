7ffd714cf067b3ef031bb7050b0977c963643288
yengsu <yengsu@yengsu.com>
2021-05-20 10:40:14 +0800

    修复linux动态链接库拷贝不完整

--- conanfile.py
+++ conanfile.py
@@ -51,7 +51,7 @@ set(CMAKE_MODULE_PATH ${CMAKE_BINARY_DIR} ${CMAKE_MODULE_PATH})''')
         self.copy("*.h", dst="include", src="exports")
         self.copy("*trantor.lib", dst="lib", keep_path=False)
         self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.so*", dst="lib", keep_path=False)
         self.copy("*.dylib", dst="lib", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
 
